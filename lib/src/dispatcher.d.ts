d8bfd52c0105efa0bbf52640e7ff57aad65eb678
ianertson <sebbekarlsson97@gmail.com>
2020-11-21 19:35:23 +0100

    rebuild

--- lib/src/dispatcher.d.ts
+++ lib/src/dispatcher.d.ts
@@ -1,26 +1,32 @@
 import { TStateEvent, FThen } from "./types";
 export declare class Dispatcher<T> {
-  listeners: {
-    [x: string]: FThen[];
-  };
-  ons: {
-    [x: string]: FThen[];
-  };
-  prevState: {
-    [x: string]: Partial<T>;
-  };
-  nextState: {
-    [x: string]: Partial<T>;
-  };
-  state: {
-    [x: string]: any;
-  };
-  onStateChange: (state: { [x: string]: any }) => any;
-  constructor(onStateChange?: (state: { [x: string]: any }) => any);
-  when(stateEvent: TStateEvent, then: FThen): FThen;
-  on(stateEvent: TStateEvent, then: FThen): FThen;
-  emit(stateEvent: TStateEvent, payload: Partial<T>): any;
-  finishEmit(nextState: { [x: string]: any }): any;
-  search(path: string): any;
-  setInitialState(path: string, state: any): void;
+    listeners: {
+        [x: string]: FThen[];
+    };
+    ons: {
+        [x: string]: FThen[];
+    };
+    prevState: {
+        [x: string]: Partial<T>;
+    };
+    nextState: {
+        [x: string]: Partial<T>;
+    };
+    state: {
+        [x: string]: any;
+    };
+    onStateChange: (state: {
+        [x: string]: any;
+    }) => any;
+    constructor(onStateChange?: (state: {
+        [x: string]: any;
+    }) => any);
+    when(stateEvent: TStateEvent, then: FThen): FThen;
+    on(stateEvent: TStateEvent, then: FThen): FThen;
+    emit(stateEvent: TStateEvent, payload: Partial<T>): any;
+    finishEmit(nextState: {
+        [x: string]: any;
+    }): any;
+    search(path: string): any;
+    setInitialState(path: string, state: any): void;
 }
--- lib/src/types.d.ts
+++ lib/src/types.d.ts
@@ -3,8 +3,8 @@ export declare type FWhen = <T>(stateEvent: TStateEvent) => Promise<T>;
 export declare type FThen = (prevState: any, state: any) => any;
 export declare type FEmit = (stateEvent: TStateEvent, state: any) => any;
 export interface IDispatcher {
-  [x: string]: {
-    state: any;
-    when: FWhen;
-  };
+    [x: string]: {
+        state: any;
+        when: FWhen;
+    };
 }
